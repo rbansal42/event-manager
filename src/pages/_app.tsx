3dcf9b867172e73515cf451928569d615838c2e0
Rahul Bansal <42.rahulbansal@gmail.com>
2024-12-27 03:50:36 +0530

    feat: add layout component with header - Create Layout component for consistent page structure - Update _app.tsx to use Layout on all pages - Ensure header appears on all top-level pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "@/components/ThemeProvider";
+import { Layout } from "@/components/Layout";
+import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <ThemeProvider>
-    <Component {...pageProps} />
+  return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   </ThemeProvider>;
 }
