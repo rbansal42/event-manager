527337055799a2a29d56f7a3694180393c501940
Rahul Bansal <42.rahulbansal@gmail.com>
2024-12-26 22:52:13 +0530

    feat: add registrant mongoose schema

---
+++ src/models/Registrant.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const RegistrantSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  type: { type: [String], enum: ['Rotarian', 'Rotaractor', 'Interactor'], required: true },
+  clubName: { type: String, required: true },
+  clubDesignation: { type: String, required: true },
+  registrationDate: { type: Date, default: Date.now },
+  checkedIn: { type: Boolean, default: false },
+  checkInTime: { type: Date }
+});
+
+export default mongoose.models.Registrant || mongoose.model('Registrant', RegistrantSchema); 
